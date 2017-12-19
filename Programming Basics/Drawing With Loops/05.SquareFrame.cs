ceca054d80dd56a9a0b5934ff9cdb7c3a2f440fe
Cvetelin <cecko04@hotmail.com>
2017-12-19 21:54:17 +0200

    Update

---
+++ Programming Basics/Drawing With Loops/05.SquareFrame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05.SquareFrame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            Console.Write(new string('+', 1));
+            Console.Write(" ");
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write("- ");
+            }
+            Console.WriteLine(new string('+', 1));
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write(new string('|', 1));
+                Console.Write(" ");
+                for (int j = 0; j < n - 2; j++)
+                {
+                    Console.Write("- ");
+                }
+                Console.Write(new string('|', 1));
+                Console.Write(" ");
+                Console.WriteLine();
+            }
+
+            Console.Write(new string('+', 1));
+            Console.Write(" ");
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write("- ");
+            }            
+            Console.Write(new string('+', 1));
+            Console.WriteLine();
+        }
+    }
+}
