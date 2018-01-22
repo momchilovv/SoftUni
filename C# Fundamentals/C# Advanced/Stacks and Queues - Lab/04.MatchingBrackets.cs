3603997ced247823d58ab419baeedbbb71305370
Cvetelin <cecko04@hotmail.com>
2018-01-22 17:03:04 +0200

    Update

---
+++ C# Fundamentals/C# Advanced/Stacks and Queues - Lab/04.MatchingBrackets.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> stackOpenBracket = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    stackOpenBracket.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int startIndex = stackOpenBracket.Pop();
+                    string contents = input.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(contents);
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/C# Advanced/Stacks and Queues - Lab/05.HotPotato.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] children = Console.ReadLine().Split();
+            int toss = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>(children);
+
+            while (queue.Count != 1)
+            {
+                for (int i = 1; i < toss; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
