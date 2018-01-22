cbfa739dfa6620acd8cdc6493f65a1691fa194fe
Cvetelin <cecko04@hotmail.com>
2018-01-22 17:29:26 +0200

    Update

---
+++ C# Fundamentals/C# Advanced/Stacks and Queues - Lab/06.TrafficLight.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.TrafficLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsPerGreen = int.Parse(Console.ReadLine());
+            Queue<string> carsQueue = new Queue<string>();
+            int carsPassedTotal = 0;
+
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    var carsThatCanPass = Math.Min(carsQueue.Count, carsPerGreen);
+                    for (int i = 0; i < carsThatCanPass; i++)
+                    {
+                        Console.WriteLine($"{carsQueue.Dequeue()} passed!");
+                        carsPassedTotal++;
+                    }
+                }
+                else
+                {
+                    carsQueue.Enqueue(input);
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{carsPassedTotal} cars passed the crossroads.");
+        }
+    }
+}
