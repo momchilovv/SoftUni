66d2b2f6682a766e69515d69101bdbb87fe8f990
Cvetelin <cecko04@hotmail.com>
2018-01-25 15:30:56 +0200

    Update

---
+++ C# Fundamentals/C# Advanced/Multidimensional Arrays - Lab/01.SumMatrixElements.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowAndCol = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToArray();
+            int[,] matrix = new int[rowAndCol[0], rowAndCol[1]];
+            for (int i = 0; i < rowAndCol[0]; i++)
+            {
+                int[] rowValues = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToArray();
+                for (int j = 0; j < rowAndCol[1]; j++)
+                {
+                    matrix[i, j] = rowValues[j];
+                }
+            }
+            int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    sum += matrix[i, j];
+                }
+            }
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+    }
+}
