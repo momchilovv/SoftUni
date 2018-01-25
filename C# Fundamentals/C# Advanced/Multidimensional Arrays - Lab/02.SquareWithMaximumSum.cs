c385a86a21045b8e671a89db762e3fe36c6ada46
Cvetelin <cecko04@hotmail.com>
2018-01-25 17:05:48 +0200

    Update

---
+++ C# Fundamentals/C# Advanced/Multidimensional Arrays - Lab/02.SquareWithMaximumSum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SquareWithMaximumSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowAndCol = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToArray();
+            int[,] matrix = new int[rowAndCol[0], rowAndCol[1]];
+            for (int i = 0; i < rowAndCol[0]; i++)
+            {
+                int[] rowValues = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToArray();
+                for (int j = 0; j < rowAndCol[1]; j++)
+                {
+                    matrix[i, j] = rowValues[j];
+                }
+            }
+            int sum = 0;
+            int rowIndex = 0, columnIndex = 0;
+            for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+                {
+                    int tempSum = matrix[i, j] + matrix[i, j + 1] +
+                                  matrix[i + 1, j] + matrix[i + 1, j + 1];
+                    if (tempSum > sum)
+                    {
+                        sum = tempSum;
+                        rowIndex = i;
+                        columnIndex = j;
+                    }
+                }
+            }
+            Console.WriteLine(matrix[rowIndex, columnIndex] + " " + matrix[rowIndex, columnIndex + 1]);
+            Console.WriteLine(matrix[rowIndex + 1, columnIndex] + " " + matrix[rowIndex + 1, columnIndex + 1]);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C# Fundamentals/C# Advanced/Multidimensional Arrays - Lab/03.GroupNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.GroupNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToArray();
+
+            int[] sizes = new int[3];
+
+            foreach (var number in numbers)
+            {
+                sizes[Math.Abs(number % 3)]++;
+            }
+            int[][] jaggedArray = new int[3][];
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                jaggedArray[i] = new int[sizes[i]];
+            }
+            int[] index = new int[3];
+            foreach (var number in numbers)
+            {
+                int remainder = Math.Abs(number % 3);
+                jaggedArray[remainder][index[remainder]] = number;
+                index[remainder]++;
+            }
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write(jaggedArray[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
