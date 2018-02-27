25ac9ba40a3a2ee35491c4f1735f368f740dfd8e
Cvetelin <cecko04@hotmail.com>
2018-02-27 16:59:03 +0200

    Update

---
+++ C# Fundamentals/C# OOP Basics/Inheritance - Lab/01.SingleInheritance/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.Eat();
+        dog.Bark();
+    }
+}
---
+++ C# Fundamentals/C# OOP Basics/Inheritance - Lab/02.MultipleInheritence/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Puppy puppy = new Puppy();
+        puppy.Eat();
+        puppy.Bark();
+        puppy.Weep();
+    }
+}
