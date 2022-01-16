d9b2d5976fa8e8b91274636456bf2b79803f2af1
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2022-01-16 17:05:40 +0200

    Uploading Conditional Statements - Lab.

---
+++ Programming Basics/Conditional Statements - Lab/04.PasswordGuess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        if (input == "s3cr3t!P@ssw0rd")
+        {
+            Console.WriteLine("Welcome");
+        }
+        else
+        {
+            Console.WriteLine("Wrong password!");
+        }
+    }
+}
