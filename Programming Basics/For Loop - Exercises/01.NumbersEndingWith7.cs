5c70de52b1133ae1d2ed2bd7e336448355b48e58
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2022-01-27 22:47:48 +0200

    Uploading For Loops - Exercises.

---
+++ Programming Basics/For Loop - Exercises/01.NumbersEndingWith7.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            if (i % 10 == 7)
+                Console.WriteLine(i);         
+        }
+    }
+}
---
+++ Programming Basics/For Loop - Exercises/02.HalfSumElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numbers = int.Parse(Console.ReadLine());
+        int sum = 0;
+        int max = 0;
+
+        for (int i = 0; i < numbers; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            sum += number;
+
+            if (number > max)
+                max = number;     
+        }
+        if ((sum - max) == max)
+            Console.WriteLine("Yes\r\n" +
+                $"Sum = {sum - max}");
+        else
+            Console.WriteLine("No\r\n" +
+                $"Diff = {Math.Abs(max - (sum - max))}");
+    }
+}
