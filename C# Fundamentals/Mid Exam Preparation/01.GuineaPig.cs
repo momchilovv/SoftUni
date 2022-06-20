e5ba6d25318e78f653e7a97670c59507772d217f
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2022-06-20 18:09:17 +0200

    Uploading Mid Exam Preparation.

---
+++ C# Fundamentals/Mid Exam Preparation/01.GuineaPig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double food = double.Parse(Console.ReadLine());
+        double hay = double.Parse(Console.ReadLine());
+        double cover = double.Parse(Console.ReadLine());
+        double pigsWeight = double.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 30; i++)
+        {
+            food -= 0.300;
+            if (i % 2 == 0)
+            {
+                hay -= food * 0.05;
+            }
+            if ((i % 3) == 0)
+            {
+                cover -= pigsWeight * 0.3333;
+            }
+
+            if (food <= 0 || hay <= 0 || cover <= 0)
+            {
+                Console.WriteLine("Merry must go to the pet store!");
+                Environment.Exit(0);
+            }
+        }
+        Console.WriteLine($"Everything is fine! Puppy is happy! Food: {food:f2}, Hay: {hay:f2}, Cover: {cover:f2}.");
+    }
+}
---
+++ C# Fundamentals/Mid Exam Preparation/02.TheLift.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int queue = int.Parse(Console.ReadLine());
+        int[] liftState = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int capacity = (liftState.Length * 4) - liftState.Sum();
+
+        for (int i = 0; i < liftState.Length; i++)
+        {
+            int max = 4 - liftState[i];
+            if (max > 0 && queue > 0 && capacity > 0)
+            {
+                for (int j = 0; j < max; j++)
+                {
+                    liftState[i]++;
+                    queue--;
+                    capacity--;
+                    if (queue == 0 || capacity == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        if (queue == 0 && capacity > 0)
+        {
+            Console.WriteLine("The lift has empty spots!");
+            Console.WriteLine(string.Join(" ", liftState));
+        }
+        else if (queue > 0)
+        {
+            Console.WriteLine($"There isn't enough space! {queue} people in a queue!");
+            Console.WriteLine(string.Join(" ", liftState));
+        }
+        else if (queue == 0 && capacity == 0)
+        {
+            Console.WriteLine(string.Join(" ", liftState));
+        }
+    }
+}
