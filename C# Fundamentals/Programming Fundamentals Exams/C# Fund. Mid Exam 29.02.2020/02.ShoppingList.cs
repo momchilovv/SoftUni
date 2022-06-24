07cc9711aae5f830fc06b4bbd06de3c94a4709d3
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2022-06-24 16:55:29 +0200

    Uploading C# Fund. Mid Exam 29.02.2020.

---
+++ C# Fundamentals/Programming Fundamentals Exams/C# Fund. Mid Exam 29.02.2020/02.ShoppingList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> groceries = Console.ReadLine().Split("!").ToList();
+
+        string[] command = Console.ReadLine().Split();
+
+        while (command[0] != "Go")
+        {
+            if (command[0] == "Urgent")
+            {
+                if (!groceries.Contains(command[1]))
+                {
+                    groceries.Insert(0, command[1]);
+                }
+            }
+            else if (command[0] == "Unnecessary")
+            {
+                if (groceries.Contains(command[1]))
+                {
+                    groceries.Remove(command[1]);
+                }
+            }
+            else if (command[0] == "Correct")
+            {
+                if (groceries.Contains(command[1]))
+                {
+                    int index = groceries.IndexOf(command[1]);
+                    groceries.Insert(index, command[2]);
+                    groceries.Remove(command[1]);
+                }
+            }
+            else if (command[0] == "Rearrange")
+            {
+                if (groceries.Contains(command[1]))
+                {
+                    groceries.Remove(command[1]);
+                    groceries.Add(command[1]);
+                }
+            }
+            command = Console.ReadLine().Split();
+        }
+        Console.WriteLine(string.Join(", ", groceries));
+    }
+}
---
+++ C# Fundamentals/Programming Fundamentals Exams/C# Fund. Mid Exam 29.02.2020/03.HeartDelivery.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> neighborhood = Console.ReadLine().Split("@").Select(int.Parse).ToList();
+        List<int> housesCelebrated = new List<int>();
+        int cupidIndex = 0;
+
+        while (true)
+        {
+            string[] command = Console.ReadLine().Split();
+
+            if (command[0] == "Love!")
+            {
+                break;
+            }
+            if (command[0] == "Jump")
+            {
+                int index = int.Parse(command[1]);
+
+                if (cupidIndex  + index > neighborhood.Count - 1)
+                {
+                    cupidIndex = 0;
+                    if (neighborhood[cupidIndex] == 0)
+                    {
+                        Console.WriteLine($"Place {cupidIndex} already had Valentine's day.");
+                    }
+                    else
+                    {
+                        neighborhood[cupidIndex] -= 2;
+                        if (neighborhood[cupidIndex] == 0)
+                        {
+                            Console.WriteLine($"Place {cupidIndex} has Valentine's day.");
+                            if (!housesCelebrated.Contains(cupidIndex))
+                            {
+                                housesCelebrated.Add(cupidIndex);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    cupidIndex += index;
+                    if (neighborhood[cupidIndex] == 0)
+                    {
+                        Console.WriteLine($"Place {cupidIndex} already had Valentine's day.");
+                    }
+                    neighborhood[cupidIndex] -= 2;
+                    if (neighborhood[cupidIndex] == 0)
+                    {
+                        Console.WriteLine($"Place {cupidIndex} has Valentine's day.");
+                        if (!housesCelebrated.Contains(cupidIndex))
+                        {
+                            housesCelebrated.Add(cupidIndex);
+                        }
+                    }
+                }
+            }
+        }
+        Console.WriteLine($"Cupid's last position was {cupidIndex}.");
+        if (neighborhood.Count == housesCelebrated.Count)
+        {
+            Console.WriteLine("Mission was successful.");
+        }
+        else
+        {
+            Console.WriteLine($"Cupid has failed {neighborhood.Count - housesCelebrated.Count} places.");
+        }
+    }
+}
