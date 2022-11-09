f6ffc6c643876468c2bf9d08d65e91de175c57f1
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2022-11-09 21:26:00 +0100

    Uploading Polymorphism - Lab.

---
+++ C# Advanced/C# OOP/Polymorphism - Lab/03.Shapes/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double width, height;
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Width
+        {
+            get { return width; }
+            private set { width = value; }
+        }
+        public double Height
+        {
+            get { return height; }
+            private set { height = value; }
+        }
+
+        public override double CalculateArea()
+        {
+            return Height * Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Height * 2 + Width * 2;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + GetType().Name;
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/Polymorphism - Lab/03.Shapes/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(3);
+
+            Console.WriteLine(circle.Draw());
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.CalculatePerimeter()); 
+            
+            Shape rectangle = new Rectangle(5, 7);
+
+            Console.WriteLine(rectangle.Draw());
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.CalculatePerimeter());
+        }
+    }
+}
