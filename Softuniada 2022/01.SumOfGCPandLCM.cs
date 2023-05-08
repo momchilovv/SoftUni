ac527a7b9fc460116b6175a8ae28abf705b732a3
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2023-05-08 20:01:43 +0200

    Uploading Sum of GCP and LMC - Softuniada 2022.

---
+++ Softuniada 2022/01.SumOfGCPandLCM.cs
@@ -0,0 +1,57 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine()!);
+        int m = int.Parse(Console.ReadLine()!);
+
+        var firstDivisiors = new List<int>();
+        var secondDivisiors = new List<int>();
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                firstDivisiors.Add(i);
+            }
+        }
+
+        for (int i = 1; i <= m; i++)
+        {
+            if (m % i == 0)
+            {
+                secondDivisiors.Add(i);
+            }
+        }
+
+        var greatestCommonDivision = firstDivisiors.Last(x => n % x == 0 && m % x == 0);
+
+        int firstNumber, secondNumber;
+
+        if (n > m)
+        {
+            firstNumber = n;
+            secondNumber = m;
+        }
+        else
+        {
+            firstNumber = m;
+            secondNumber = n;
+        }
+
+        int lowestCommonMultiple = firstNumber * secondNumber;
+
+        for (int i = 1; i < secondNumber; i++)
+        {
+            int multiple = firstNumber * i;
+
+            if (multiple % secondNumber == 0)
+            {
+                lowestCommonMultiple = multiple;
+                break;
+            }
+        }
+
+        Console.WriteLine(greatestCommonDivision + lowestCommonMultiple);
+    }
+}
