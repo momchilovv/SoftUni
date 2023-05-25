300ea3e4db4f45a84fcb124420865ddb8c61a35b
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2023-05-25 21:51:39 +0200

    Uploading Text Splitter App.

---
+++ C# Web/Text Splitter App/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TextSplitterApp.Models;
+
+namespace TextSplitterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(TextViewModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextViewModel model)
+        {
+            var splittedText = model.Text
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            model.SplitText = String.Join(Environment.NewLine, splittedText);
+
+            return RedirectToAction("Index", model);
+        }
+    }
+}
---
+++ C# Web/Text Splitter App/Models/TextViewModel.cs
@@ -0,0 +1,16 @@
+﻿#nullable disable
+using System.ComponentModel.DataAnnotations;
+
+namespace TextSplitterApp.Models
+{
+    public class TextViewModel
+    {
+        [Required(ErrorMessage = "The text field is required!")]
+        [StringLength(30, MinimumLength = 2,
+            ErrorMessage = "The text field must be a string with a minimum length of 2 and maximum length of 30.")]
+        
+        public string Text { get; set; }
+
+        public string SplitText { get; set; }
+    }
+}
